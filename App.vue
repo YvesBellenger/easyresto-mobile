d6a1d09b07d365fd8460b49a87216b7d2bcbfccb
Yves BELLENGER <yvesbellenger@hotmail.com>
2018-07-02 12:39:03 +0200

    first commit

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="container">
+    <text class="welcome">{{message}}</text>
+  </view>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        message: "Welcome to Vue Native!!"
+      }
+    }
+  }
+</script>
+
+<style>
+.container {
+  background-color: #333;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.welcome {
+  color: white;
+  font-size: 20px;
+  margin: 20px;
+}
+</style>
